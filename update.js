421b99b5bca86ca9167e48a3f7c36eb58d54aba2
Michael Paukov <michael.paukov@gmail.com>
2023-02-22 14:01:28 +0100

    continue

--- update.js
+++ update.js
@@ -8,103 +8,20 @@ const url = `${URL_BC}`;
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    userDataDir: "./.temp",
-  });
-
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  // await page.tracing.start({
-  //   // categories: ["devtools.timeline"],
-  //   path: "./.temp/.tracing.json",
-  // });
+  await page.setViewport({ width: 1400, height: 1024 });
   await page.goto(url);
-  // await page.goto("http://example.com/");
 
   const responses = [];
-
-  page.on("response", async (response) => {
-    if (response.request().resourceType() === "xhr") {
-      const responseHeaders = response.headers();
-      const contentType = responseHeaders["content-type"];
-      if (contentType && contentType.includes("application/json")) {
-        const responseData = await response.json();
-        responses.push(responseData);
-      }
-    }
-  });
-
-  // Wait for the page to finish loading and all network requests to complete
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
-
-  // const urls = [];
-  // page.on("response", (response) => {
-  //   const url = response.url();
-  //   urls.push(url);
-  // if (
-  //   url.endsWith(".js") ||
-  //   url.endsWith(".css") ||
-  //   url.endsWith(".png") ||
-  //   url.endsWith(".jpg")
-  // ) {
-  //   urls.push(url);
-  // }
-  // });
-
-  // Wait for the page to finish loading and all network requests to complete
-
-  console.log(responses);
-  // await page.waitForNavigation({ waitUntil: "networkidle2" });
-
-  // console.log(urls);
-
-  // await page.goto("https://developer.chrome.com/");
-
-  // Set screen size
-  await page.setViewport({ width: 1400, height: 1024 });
-  const cookies = JSON.stringify(await page.cookies());
-  const sessionStorage = await page.evaluate(() =>
-    JSON.stringify(sessionStorage)
-  );
-  const localStorage = await page.evaluate(() => JSON.stringify(localStorage));
-
-  // console.log("cookies", cookies);
-  // console.log("sessionStorage", sessionStorage);
-  // console.log("localStorage", localStorage);
-
-  // var tracing = JSON.parse(await page.tracing.stop());
-
-  // const ttt = tracing.traceEvents.filter(
-  //   (te) => te.name === "ResourceReceiveResponse"
+  page.on("load", (response) => responses.push(response));
+  await page.reload();
+  responses.forEach((response) => console.log("response", response));
+  // const cookies = JSON.stringify(await page.cookies());
+  // const sessionStorage = await page.evaluate(() =>
+  //   JSON.stringify(sessionStorage)
   // );
-
-  // const ddd = ttt.filter((t) => t.args.data.mimeType.includes("png"));
-  // console.log("ddd.data", ddd);
-
-  // let performanceTiming = JSON.parse(
-  //   await page.evaluate(() => JSON.stringify(window.performance.getEntries()))
-  // );
-
-  // console.log(performanceTiming);
-
-  // Type into search box
-  // await page.type(".search-box__input", "automate beyond recorder");
-
-  // Wait and click on first result
-  // const searchResultSelector = ".search-box__link";
-  // await page.waitForSelector(searchResultSelector);
-  // await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  // const textSelector = await page.waitForSelector(
-  //   "text/Customize and automate"
-  // );
-  // const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-  // Print the full title
-  // console.log('The title of this blog post is "%s".', fullTitle);
-
-  // await browser.close();
+  // const localStorage = await page.evaluate(() => JSON.stringify(localStorage));
 })();
 
 // const instance = axios.create({
